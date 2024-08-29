bd5f2828d6cfaeffb13bf45653592f57523e652d
shotoh <trueshotoh@gmail.com>
2024-08-28 23:51:00 -0700

    spring boot

---
+++ server/src/main/java/io/github/shotoh/uzi/UziApplication.java
@@ -0,0 +1,13 @@
+package io.github.shotoh.uzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UziApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/github/shotoh/uzi/UziApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shotoh.uzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
