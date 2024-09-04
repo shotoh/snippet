ea38fb90982b18314f9f1c83041f37b838a8d686
PattyCakesxD <nguyenpatty42@gmail.com>
2024-09-03 20:32:25 -0700

    adds tailwindcss dependencies+files

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,13 @@
-import { Button } from "@headlessui/react";
+import React from "react";
 
 function App() {
-  return <Button>Whats up</Button>;
+  return (
+    <div className="">
+      <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+        sacdas,dca9wdcwa
+      </button>
+    </div>
+  );
 }
 
 export default App;
