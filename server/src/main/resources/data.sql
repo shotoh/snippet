3a25970edcd25ebe42870974391e40992f5d15ec
shotoh <trueshotoh@gmail.com>
2024-09-03 22:57:32 -0700

    insert dummy data to db

---
+++ server/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    `user` (name, email)
+VALUES
+    ('Spongebob Squarepants', 'spongebob@gmail.com'),
+    ('Patrick Star', 'patrick@gmail.com'),
+    ('Squidward Tentacles', 'squidward@gmail.com');
