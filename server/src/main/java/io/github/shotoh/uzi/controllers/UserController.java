39224699ab7d92939eb3e6ce8e3e3fdbfb0cc218
shotoh <trueshotoh@gmail.com>
2024-09-06 00:15:31 -0700

    updated api and proxy urls

--- server/src/main/java/io/github/shotoh/uzi/controllers/UserController.java
+++ server/src/main/java/io/github/shotoh/uzi/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping(path = "/api/users")
 public class UserController {
     private final UserService service;
 
