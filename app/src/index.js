e8a4f33f6d9cba21b731e034eaa51f2e3fe5988f
shotoh <trueshotoh@gmail.com>
2024-09-05 23:33:56 -0700

    remove axios

--- app/src/index.js
+++ app/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
 //import { BrowserRouter } from "react-router-dom";
-
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
