2879b1e86f63d7269bfcc73d4576da58a7e996fb
croquet-flamingo <JB.Meep.09@gmail.com>
2024-09-05 21:06:41 -0700

    separated pages

---
+++ app/src/pages/SocialMedia.js
@@ -0,0 +1,8 @@
+
+import { SomeButton } from "../components/SomeButton.js";
+
+export default function SocialMedia() {
+    return <div className="flex min-h-screen justify-center items-center bg-gradient-to-br from-blue-800 to-cyan-600">
+    <SomeButton />
+  </div>
+}
