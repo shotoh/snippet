6848202b19f1767af2c44306252b2eec4497969b
shotoh <trueshotoh@gmail.com>
2024-09-10 17:23:59 -0700

    updated validation for user

--- server/src/main/java/io/github/shotoh/uzi/exceptions/UziExceptionHandler.java
+++ server/src/main/java/io/github/shotoh/uzi/exceptions/UziExceptionHandler.java
@@ -1,7 +1,13 @@
 package io.github.shotoh.uzi.exceptions;
 
+import jakarta.validation.ValidationException;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.method.MethodValidationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,8 +23,14 @@ public class UziExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
-    @ExceptionHandler(value = ValidationException.class)
-    public ResponseEntity<String> exception(ValidationException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> exception(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            if (error instanceof FieldError fieldError) {
+                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
--- server/src/main/java/io/github/shotoh/uzi/models/User.java
+++ server/src/main/java/io/github/shotoh/uzi/models/User.java
@@ -1,28 +1,53 @@
 package io.github.shotoh.uzi.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import java.time.Instant;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.Instant;
-
 @Entity
 @Getter @Setter @NoArgsConstructor
 public class User {
+    private static final int USERNAME_MIN_LEN = 3;
+    private static final int USERNAME_MAX_LEN = 31;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(unique = true, nullable = false, updatable = false)
+    @NotBlank
+    @Size(min = USERNAME_MIN_LEN, max = USERNAME_MAX_LEN)
     private String username;
+
     @Column(unique = true, nullable = false, updatable = false)
+    @NotBlank
+    @Email
     private String email;
+
     @Column(nullable = false)
+    @NotBlank
     private String encryptedPass;
+
+    @Size(min = USERNAME_MIN_LEN, max = USERNAME_MAX_LEN)
     private String displayName;
+
     private String profilePicture;
+
     private String biography;
+
     @Column(nullable = false)
+    @NotBlank
+    @PositiveOrZero
     private long timestamp = Instant.now().toEpochMilli();
 }
