5ddcabbebbe5def25e39d38d1b57b4e1ff8889fa
shotoh <trueshotoh@gmail.com>
2024-09-16 10:06:58 -0700

    fixed media api route name

--- server/src/main/java/io/github/shotoh/uzi/controllers/MediaController.java
+++ server/src/main/java/io/github/shotoh/uzi/controllers/MediaController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/medias")
+@RequestMapping(path = "/api/media")
 public class MediaController {
     private final MediaService service;
 
