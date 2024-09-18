af63c0053767346d503ea4323fb650b112118e1e
FerociousBlade <elias.aceves04@gmail.com>
2024-09-18 09:22:01 -0700

    New buttons added

--- app/src/pages/Main.js
+++ app/src/pages/Main.js
@@ -2,7 +2,7 @@
 import { SomeButton } from "../components/SomeButton.js";
 
 export default function Main() {
-    return <div className="flex min-h-screen justify-center items-center bg-gradient-to-br from-blue-800 to-cyan-600">
+    return <div className="flex min-h-screen justify-center items-start pt-12 bg-gradient-to-br from-blue-800 to-cyan-600">
     <SomeButton />
   </div>
-}
+} 
