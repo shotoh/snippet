53d0b4aa97607ed7ad3a488a5174b365d901872c
FerociousBlade <elias.aceves04@gmail.com>
2024-09-19 19:22:54 -0700

    Added button styling, need table style

--- app/src/components/SomeButton.js
+++ app/src/components/SomeButton.js
@@ -36,15 +36,157 @@ export const SomeButton = () => {
   return (
     <div className="flex flex-col items-center space-y-4">
       <div className="flex space-x-4">
-        {/* Buttons for each endpoint */}
-        <button onClick={() => handleClick('users')}>Users</button>
-        <button onClick={() => handleClick('posts')}>Posts</button>
-        <button onClick={() => handleClick('comments')}>Comments</button>
-        <button onClick={() => handleClick('post-likes')}>Post Likes</button>
-        <button onClick={() => handleClick('comment-likes')}>Comment Likes</button>
-        <button onClick={() => handleClick('friends')}>Friends</button>
-        <button onClick={() => handleClick('messages')}>Messages</button>
-        <button onClick={() => handleClick('media')}>Media</button>
+        <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+        onClick={() => handleClick('users')}
+        >
+          Users
+          </button>
+          
+          <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+        onClick={() => handleClick('posts')}
+        >
+          Posts
+          </button>
+
+          <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+         onClick={() => handleClick('comments')}
+         >
+          Comments
+          </button>
+
+        <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+         onClick={() => handleClick('post-likes')}
+         >
+          Post Likes
+          </button>
+
+          <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+         onClick={() => handleClick('comment-likes')}
+         >
+          Comment Likes
+          </button>
+
+          <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+        onClick={() => handleClick('friends')}
+        >
+          Friends
+          </button>
+
+          <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500"
+         onClick={() => handleClick('messages')}
+         >
+          Messages
+          </button>
+
+          <button
+        className="relative
+                overflow-hidden
+                rounded-md
+                shadow-md
+                shadow-black/20
+                bg-gradient-to-r from-green-700 to-lime-600
+                py-3 px-4
+                text-lg
+                font-medium
+                uppercase
+                tracking-wider
+                text-white
+                hover:from-green-600 hover:to-lime-500" 
+        onClick={() => handleClick('media')}
+        >
+          Media
+          </button>
       </div>
 
       {error && <p className="text-center text-red-500 mt-4">Error: {error}</p>}
