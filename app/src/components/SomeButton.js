40869ac36c370ae94cd5fc2b77a9774483fd3fe5
PattyCakesxD <nguyenpatty42@gmail.com>
2024-09-19 19:43:00 -0700

    condense button into a component

--- app/src/components/SomeButton.js
+++ app/src/components/SomeButton.js
@@ -10,6 +10,8 @@ import {
   MediaTable,
 } from "./Tables";
 
+import { NavButton } from "./NavButton";
+
 export const SomeButton = () => {
   const [fetchedData, setFetchedData] = useState([]);
   const [error, setError] = useState(null);
@@ -36,170 +38,49 @@ export const SomeButton = () => {
   return (
     <div className="flex flex-col items-center space-y-4">
       <div className="flex space-x-4">
-        <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-        onClick={() => handleClick('users')}
-        >
-          Users
-          </button>
-          
-          <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-        onClick={() => handleClick('posts')}
-        >
-          Posts
-          </button>
-
-          <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-         onClick={() => handleClick('comments')}
-         >
-          Comments
-          </button>
-
-        <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-         onClick={() => handleClick('post-likes')}
-         >
-          Post Likes
-          </button>
-
-          <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-         onClick={() => handleClick('comment-likes')}
-         >
-          Comment Likes
-          </button>
-
-          <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-        onClick={() => handleClick('friends')}
-        >
-          Friends
-          </button>
-
-          <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500"
-         onClick={() => handleClick('messages')}
-         >
-          Messages
-          </button>
-
-          <button
-        className="relative
-                overflow-hidden
-                rounded-md
-                shadow-md
-                shadow-black/20
-                bg-gradient-to-r from-green-700 to-lime-600
-                py-3 px-4
-                text-lg
-                font-medium
-                uppercase
-                tracking-wider
-                text-white
-                hover:from-green-600 hover:to-lime-500" 
-        onClick={() => handleClick('media')}
-        >
-          Media
-          </button>
+        <NavButton text="Users" onClick={() => handleClick("users")} />
+        <NavButton text="Posts" onClick={() => handleClick("posts")} />
+        <NavButton text="Comments" onClick={() => handleClick("comments")} />
+        <NavButton
+          text="Post Likes"
+          onClick={() => handleClick("post-likes")}
+        />
+        <NavButton
+          text="Comment Likes"
+          onClick={() => handleClick("comment-likes")}
+        />
+        <NavButton text="Friends" onClick={() => handleClick("friends")} />
+        <NavButton text="Messages" onClick={() => handleClick("messages")} />
+        <NavButton text="Media" onClick={() => handleClick("media")} />
       </div>
 
       {error && <p className="text-center text-red-500 mt-4">Error: {error}</p>}
 
       {/* Conditionally render the correct table based on the selected endpoint */}
-      {fetchedData.length > 0 && currentEndpoint === "users" && <UserTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "posts" && <PostTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "comments" && <CommentTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "post-likes" && <PostLikesTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "comment-likes" && <CommentLikesTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "friends" && <FriendsTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "messages" && <MessagesTable data={fetchedData} />}
-      {fetchedData.length > 0 && currentEndpoint === "media" && <MediaTable data={fetchedData} />}
+      {fetchedData.length > 0 && currentEndpoint === "users" && (
+        <UserTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "posts" && (
+        <PostTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "comments" && (
+        <CommentTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "post-likes" && (
+        <PostLikesTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "comment-likes" && (
+        <CommentLikesTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "friends" && (
+        <FriendsTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "messages" && (
+        <MessagesTable data={fetchedData} />
+      )}
+      {fetchedData.length > 0 && currentEndpoint === "media" && (
+        <MediaTable data={fetchedData} />
+      )}
     </div>
   );
 };
