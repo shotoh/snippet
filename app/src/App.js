0341103be6bb061cbb3dbd7bcadcc4499093fecf
Patrick Hoang <nguyenpatty42@gmail.com>
2024-09-19 20:21:24 -0700

    Some small fixes (#23)
    * prettier and fix mainnav case sensitivity
    
    * change snipit to snippet
    
    * fix snippet link

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.js";
 import Products from "./pages/Products.js";
 import Contact from "./pages/Contact.js";
@@ -11,23 +11,20 @@ import SimpleBody from "./components/SimpleBody.js";
 function App() {
   //let component;
 
-
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}>
-          <Route path="" element={<SimpleBody /> } />
+          <Route path="" element={<SimpleBody />} />
         </Route>
-        <Route path="/home" element={<Home />} >
-            <Route path="products" element={<Products />} />
-            <Route path="about" element={<About />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="" element={<SimpleBody /> } />
+        <Route path="/home" element={<Home />}>
+          <Route path="products" element={<Products />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="" element={<SimpleBody />} />
         </Route>
-        <Route path="/snipit" element={<Main />} />
+        <Route path="/snippet" element={<Main />} />
       </Routes>
-      
     </div>
   );
 }
--- app/src/pages/Contact.js
+++ app/src/pages/Contact.js
@@ -1,16 +1,8 @@
-
-
 export default function Contact() {
-    
-    
-    
-    
-    return (
-        <section id="contact" className="p-4 space-y-4">
-            <h1 className="text-4xl font-bold">Contact Methods</h1>
-                <p>Do not contact us.</p>
-            </section>
-        
-        
-    );
+  return (
+    <section id="contact" className="p-4 space-y-4">
+      <h1 className="text-4xl font-bold">Contact Methods</h1>
+      <p>Do not contact us.</p>
+    </section>
+  );
 }
