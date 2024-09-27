54587e72e3c0ca09dcaa3b7ff2fc684b7cc5b4a8
PattyCakesxD <nguyenpatty42@gmail.com>
2024-09-27 10:08:43 -0700

    adds containers for other sections

--- app/src/pages/MainPage/MainPage.js
+++ app/src/pages/MainPage/MainPage.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 import NavBar from "../../components/MainPage/NavBar";
-import FriendsBar from "../../components/MainPage/FriendsBar";
-import Feed from "../../components/MainPage/Feed";
 import TrendingBar from "../../components/MainPage/TrendingBar";
+import Feed from "../../components/MainPage/Feed";
+import FriendsBar from "../../components/MainPage/FriendsBar";
 
 const MainPage = () => {
   return (
-    <div className="min-h-screen bg-slate-200">
+    <div className="min-h-screen bg-slate-200 flex flex-col">
       <NavBar />
+      <div className="flex-grow grid grid-cols-12 gap-4 mt-4">
+        <div className="col-span-4 bg-orange-400">
+          <TrendingBar />
+        </div>
+
+        <div className="col-span-5 px-4 bg-sky-500">
+          <Feed />
+        </div>
+
+        <div className="col-span-3 mr-4 bg-purple-400">
+          <FriendsBar />
+        </div>
+      </div>
     </div>
   );
 };
