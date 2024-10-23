6d6bc5d9213f20355f0e580c89351d64ffe0cf8a
PattyCakesxD <nguyenpatty42@gmail.com>
2024-10-23 10:31:55 -0700

    basic redesign of profile page

--- app/src/components/ProfilePage/ProfileBanner.jsx
+++ app/src/components/ProfilePage/ProfileBanner.jsx
@@ -6,16 +6,13 @@ export default function ProfileBanner() {
       <img
         src={require("../../images/somepicture.jpg")}
         alt="Profile Banner"
-        className="w-full h-44 object-cover"
+        className="w-full h-32 md:h-44 object-cover"
       />
-
-      <div className="absolute -bottom-24 left-12">
-        {" "}
-        {/* Left margin added here */}
+      <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2">
         <img
           src={require("../../images/jackblack.jpg")}
           alt="Profile"
-          className="rounded-full w-48 h-48 object-cover"
+          className="rounded-full w-32 h-32 md:w-40 md:h-40 object-cover border-4 border-white"
         />
       </div>
     </div>
--- app/src/components/ProfilePage/ProfileInfo.jsx
+++ app/src/components/ProfilePage/ProfileInfo.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 const loremIpsum =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 export default function ProfileInfo() {
   return (
-    <div className="mt-28 ml-12">
+    <div className="mt-20 md:mt-28 px-8">
       <h1 className="text-3xl font-bold font-montserrat">Some Guy</h1>
       <p className="text-xl text-gray-500 font-montserrat">@someguy1</p>
-      <p className="mt-4 text-gray-700 font-montserrat max-w-lg">
+      <p className="mt-4 text-gray-700 font-montserrat max-w-lg mx-auto">
         {loremIpsum}
       </p>
     </div>
