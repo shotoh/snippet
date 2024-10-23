824b0337aace5169d042be2e1b222581724902d6
PattyCakesxD <nguyenpatty42@gmail.com>
2024-10-23 00:26:12 -0700

    changed styling of trending bar (temp)

--- app/src/components/MainPage/TrendingBar.jsx
+++ app/src/components/MainPage/TrendingBar.jsx
@@ -2,44 +2,36 @@ import React from "react";
 import TrendingCard from "./TrendingCard.jsx";
 
 export default function TrendingBar({ posts, error }) {
-  
-  const fetchPosts = async (event) => {
-      
-  }
+  const fetchPosts = async (event) => {};
 
+  const createTrendingCard = () => {};
 
-  const createTrendingCard = () => {
-  }
-  
-  
   return (
-    <div className="w-full h-full">
-      <div className="mx-auto pt-2">
-        <h3 className="text-center">See What's</h3>
-        <h1 className="text-center "><b>Trending</b></h1>
+    <div className="w-full h-full px-3">
+      <div className="text-center py-4 font-montserrat leading-tight text-black border-b border-gray-400">
+        <span className="text-2xl">See what's</span> <br />
+        <span className="text-5xl font-bold">Trending</span>
       </div>
-      <div id="TrendingContentCreator" className="px-2">
-        <TrendingCard 
+      <div id="TrendingContentCreator">
+        <TrendingCard
           backgroundImage="https://variety.com/wp-content/uploads/2018/12/Jack-Black.jpg"
           circleImage="https://yt3.googleusercontent.com/ytc/AIdro_mv2AHfwkrm0yOrdapvdVzoW0x3t-pBnADG-VXB3uqqeHo=s160-c-k-c0x00ffffff-no-rj"
           userURL={"/login"}
         />
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {posts.length === 0 ? (
-        <p>No posts available</p>
-      ) : (
-        posts.map((post) => (
-          <TrendingCard
-            backgroundImage={post.backgroundImage}
-            circleImage={post.circleImage}
-            postURL={post.postURL}
-            userURL={post.userURL}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {posts.length === 0 ? (
+          <p>No posts available</p>
+        ) : (
+          posts.map((post) => (
+            <TrendingCard
+              backgroundImage={post.backgroundImage}
+              circleImage={post.circleImage}
+              postURL={post.postURL}
+              userURL={post.userURL}
             />
-        ))
-      )}
+          ))
+        )}
       </div>
-      
     </div>
-    
   );
 }
