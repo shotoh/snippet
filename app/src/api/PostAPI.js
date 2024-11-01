8db4011fd1231dee5829d65e8e072f247d83645a
Patrick Hoang <nguyenpatty42@gmail.com>
2024-10-31 20:42:43 -0700

    Patrick/postcard (#58)
    * adds missing if statement (accidently removed during merge)
    
    * very basic template on a post (needs proper PostAPI implementation)
    
    * implements retrieval of all necessary post details
    
    * eslint fixes
    
    * adds image functionality to posts

---
+++ app/src/api/PostAPI.js
@@ -0,0 +1,144 @@
+import { getPostImage } from "./ImageAPI";
+
+/**
+ * Retrieve all posts from the server
+ */
+export async function fetchPosts(token) {
+  if (!token) {
+    throw new Error("No token provided");
+  }
+
+  try {
+    const response = await fetch("/api/posts", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to retrieve posts");
+    }
+
+    const result = await response.json();
+
+    if (response.ok && result.status === "success") {
+      result.forEach((post) => {
+        getFullPost(post.id, token);
+      });
+      return result.data;
+    } else {
+      throw new Error("Error fetching posts");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * Retrieve a post object along with additional details
+ * @returns - { user: { name, profilePicture }, media, text, likes, dislikes, comments }
+ */
+export async function getFullPost(postID, token) {
+  const fullPost = {
+    id: null,
+    user: { name: "", profilePicture: null },
+    media: null,
+    text: "",
+    likes: 0,
+    dislikes: 0,
+    comments: [],
+  };
+
+  if (!token) {
+    throw new Error("No token provided");
+  }
+
+  // ** (Maybe update?) { id, user, content }
+  const response = await fetch(`/api/posts/${postID}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to retrieve post");
+  }
+
+  const result = await response.json();
+
+  fullPost.id = result.data.id;
+  fullPost.user.name = result.data.user.username; // ** Replace with displayName when implemented
+  // **   fullPost.user.profilePicture =
+
+  const media = await getPostImage(postID, token);
+  fullPost.media = media;
+
+  fullPost.text = result.data.content;
+
+  // { likes, dislikes }
+  const ratings = await getPostRatings(postID, token);
+  fullPost.likes = ratings.likes;
+  fullPost.dislikes = ratings.dislikes;
+
+  // { comments }
+  const comments = await getPostComments(postID, token);
+  fullPost.comments = comments;
+
+  return fullPost;
+}
+
+/**
+ * Retrieve the likes and dislikes of a post
+ * @returns - { likes, dislikes }
+ */
+export async function getPostRatings(postID, token) {
+  const postRating = {
+    likes: 0,
+    dislikes: 0,
+  };
+
+  if (!token) {
+    throw new Error("No token provided");
+  }
+
+  try {
+    // Get post likes
+    const likesResponse = await fetch(`/api/post-likes?post=${postID}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (likesResponse.ok) {
+      const result = await likesResponse.json();
+      if (result.status === "success") {
+        postRating.likes = result.data.length;
+      }
+    } else {
+      console.error("Failed to fetch likes");
+    }
+  } catch (error) {
+    console.error("Error fetching likes:", error);
+  }
+
+  return postRating;
+}
+
+/**
+ * Retrieve all comments for a post
+ */
+export async function getPostComments(postID, token) {
+  if (!token) {
+    throw new Error("No token provided");
+  }
+
+  const response = await fetch(`/api/comments?post=${postID}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const result = await response.json();
+  return result.data;
+}
--- app/src/components/ProfilePage/ProfileFeed.jsx
+++ app/src/components/ProfilePage/ProfileFeed.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PostCard from "../MainPage/PostCard";
 
 export default function ProfileFeed({ posts, error }) {
   if (error) {
     return <p className="text-red-500">{error}</p>;
   }
 
-  if (posts.length == 0) {
+  if (posts.length === 0) {
     return <p>No posts available</p>;
   }
 
@@ -13,9 +14,17 @@ export default function ProfileFeed({ posts, error }) {
     <div>
       <h1>Feed</h1>
       {posts.map((post) => (
-        <div key={post.id} className="border-b-2 border-gray-300 py-4">
-          <h3 className="font-bold">{post.title}</h3>
-          <p>{post.content}</p>
+        <div key={post.id} className="py-3">
+          <PostCard
+            post={{
+              user: post.user, // ** TODO: profilePicture needs to be figured out in the backend, update this accordingly
+              media: post.media, // ** TODO: Images need to be figured out
+              text: post.text,
+              likes: post.likes,
+              dislikes: post.dislikes,
+              comments: post.comments,
+            }}
+          />
         </div>
       ))}
     </div>
