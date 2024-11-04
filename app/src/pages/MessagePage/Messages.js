7f23e867e106b77fb4aeb7b55bcb0c7715a17070
FerociousBlade <elias.aceves04@gmail.com>
2024-11-03 19:42:07 -0800

    abstracted mainpage and messages

--- app/src/pages/MessagePage/Messages.js
+++ app/src/pages/MessagePage/Messages.js
@@ -7,6 +7,13 @@ import MessageBody from "../../components/MessagePage/MessageBody";
 import MessageBar from "../../components/MessagePage/MessageBar";
 import FriendCard from "../../components/MessagePage/FriendCard";
 import defaultProfile from "../../images/defaultprofile.png";
+import {
+  fetchUserDetails,
+  fetchFriends,
+  fetchMessages,
+  sendMessage,
+  getToken
+} from "../../api/MessageAPI";
 
 export default function MessagesPage() {
   // Friends of user
@@ -21,173 +28,67 @@ export default function MessagesPage() {
   const [messages, setMessages] = useState([]);
   const [shouldScrollToBottom, setShouldScrollToBottom] = useState(false);
 
-  const authToken = localStorage.getItem("authToken");
-
-  // Helper function to parse JWT token
-  const parseJwt = (token) => {
-    try {
-      const base64Url = token.split(".")[1];
-      const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-      return JSON.parse(window.atob(base64));
-    } catch (error) {
-      return null;
-    }
-  };
-
   useEffect(() => {
-    // Retrieve the username of the authenticated user
-    async function fetchUserDetails() {
-      const token = authToken;
-      if (!token) {
-        setError("User is not authenticated");
-        setLoading(false);
-        return;
-      }
-
-      // Get user ID from token and set it in state
-      const userIdFromToken = parseInt(parseJwt(token).sub);
-      setUserId(userIdFromToken);
-
+    //Load user data and friends 
+    const loadUserData = async () => {
       try {
-        const response = await fetch(`/api/users/${userIdFromToken}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const { userId, username } = await fetchUserDetails();
+        setUserId(userId);
+        setUsername(username);
 
-        const result = await response.json();
-        if (response.ok && result.status === "success") {
-          setUsername(result.data.username); // Set the username
-        } else {
-          setError("Error loading user details");
-        }
-      } catch (err) {
-        setError("Error loading user details");
-      }
-    }
-
-    fetchUserDetails();
-  }, []);
-
-  useEffect(() => {
-    // Retrieve list of friends associated with user
-    async function fetchFriends() {
-      const token = authToken;
-      if (!token) {
-        setError("User is not authenticated");
-        setLoading(false);
-        return;
-      }
-
-      console.log(token);
-      console.log("[USER ID]:", userId);
-
-      // Get user ID from token and set it in state
-      const userIdFromToken = parseInt(parseJwt(token).sub);
-      setUserId(userIdFromToken);
-
-      try {
-        const response = await fetch(`/api/friends?from=${userIdFromToken}`, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const result = await response.json();
-
-        if (response.ok && result.status === "success") {
-          // Parse specifically the friends from response
-          const friendsList = result.data.filter(
-            (friend) => friend.status === "FRIEND" // Status should be "FRIEND", not "PENDING"
-          );
-
-          // Extract just the friend's id, name, and picture
-          const friendData = friendsList.map((friend) => ({
-            id: friend.to.id,
-            username: friend.to.username,
-            displayName: friend.to.username, // *** TEMPORARY: Change to displayName once implemented ***
-            profilePicture: friend.to.profilePicture || defaultProfile,
-          }));
-
-          setFriends(friendData);
-          console.log("[FRIENDS]", friendData);
-        } else {
-          setError("Error loading friends");
-        }
-      } catch (err) {
-        setError("Error loading friends");
+        const friendsData = await fetchFriends();
+        setFriends(friendsData);
+      } catch (error) {
+        setError(error.message);
       } finally {
         setLoading(false);
       }
-    }
+    };
 
-    fetchFriends();
+    loadUserData();
   }, []);
 
-  // Load/unload messages whenever a friend is selected
   useEffect(() => {
     let intervalId;
     if (selectedFriend) {
-      loadMessages(true); // User selected a friend, should scroll
-      // Start polling every 5 seconds
+      loadMessages(true);
       intervalId = setInterval(() => {
-        loadMessages(false); // Interval call, should not scroll
+        loadMessages(false);
       }, 5000);
     } else {
       setMessages([]);
     }
 
-    return () => {
-      clearInterval(intervalId);
-    };
+    return () => clearInterval(intervalId);
   }, [selectedFriend]);
 
   // Load messages for selected friend
   const loadMessages = async (shouldScroll = false) => {
-    if (!userId) {
+    if (!userId || !selectedFriend) {
       console.error("User ID is not available.");
       return;
     }
 
-    const token = authToken;
-    if (!token) {
-      setError("Token has expired. Please log in again.");
-      return;
-    }
-
     try {
-      const response = await fetch(
-        `/api/messages?from=${userId}&to=${selectedFriend.id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      const result = await response.json();
-
-      if (response.ok && result.status === "success") {
-        const newMessages = result.data;
-
-        // Check if messages have changed
-        const messagesChanged =
-          JSON.stringify(messages) !== JSON.stringify(newMessages);
-
-        if (messagesChanged) {
-          setMessages(newMessages);
-          setShouldScrollToBottom(shouldScroll);
-        }
-      } else {
-        setError("Error loading messages");
+      const newMessages = await fetchMessages(userId, selectedFriend.id);
+      if (JSON.stringify(messages) !== JSON.stringify(newMessages)) {
+        setMessages(newMessages);
+        setShouldScrollToBottom(shouldScroll);
       }
-    } catch (err) {
-      setError("Error loading messages");
+    } catch (error) {
+      setError("Error loading message");
     }
   };
 
   // Handler for new messages sent by the user
-  const handleNewMessage = (newMessage) => {
-    setMessages((prevMessages) => [...prevMessages, newMessage]);
-    setShouldScrollToBottom(true); // Scroll to bottom when user sends a message
+  const handleNewMessage = async (messageContent) => {
+    try {
+      const newMessage = await sendMessage(userId, selectedFriend.id, messageContent);
+      setMessages((prevMessages) => [...prevMessages, newMessage]);
+      setShouldScrollToBottom(true); // Scroll to bottom when user sends a message
+    } catch (error) {
+      setError("Error sending message");
+    }
   };
 
   return (
@@ -249,7 +150,7 @@ export default function MessagesPage() {
                 <MessageBar
                   selectedFriend={selectedFriend}
                   userId={userId}
-                  authToken={authToken}
+                  authToken={getToken()}
                   onNewMessage={handleNewMessage}
                 />
               </>
