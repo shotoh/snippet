b6e86f64d1180c93aaed2d92aaa6edc6543b741e
shotoh <trueshotoh@gmail.com>
2024-11-05 17:51:26 -0800

    change commentDTO to have like status

--- server/src/main/java/to/us/snippet/commentlikes/CommentLikeRepository.java
+++ server/src/main/java/to/us/snippet/commentlikes/CommentLikeRepository.java
@@ -6,4 +6,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
 	void deleteCommentLikeByUserIdAndCommentId(long userId, long commentId);
+
+	int countAllByCommentIdAndValue(long commentId, long value);
+
+	CommentLike getCommentLikeByUserIdAndCommentId(long userId, long commentId);
 }
